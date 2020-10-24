c68976ac738187f4297c3db0f7c59bbd8cb36703
Sai Sukesh <sukesh.sai04@gmail.com>
2020-10-24 18:16:06 +0530

    Correction in task 2

--- app/src/main/java/com/iettfd/tasks/ui/Task2Fragment.java
+++ app/src/main/java/com/iettfd/tasks/ui/Task2Fragment.java
@@ -35,25 +35,46 @@ public class Task2Fragment extends Fragment {
         ButterKnife.bind(this,root);
 
         AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
-        fadeIn.setDuration(4000);
-        helloText.startAnimation(fadeIn);
+        fadeIn.setDuration(2000);
 //        fadeIn.setRepeatCount(Animation.INFINITE);
+
+        AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeOut.setDuration(2000);
+//        fadeIn.setRepeatCount(Animation.INFINITE);
+
         fadeIn.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                helloText.animate().rotation(360f).setDuration(4000);
+                helloText.animate().rotation(360).setDuration(2000);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                helloText.animate().rotation(-360f).setDuration(4000);
+                helloText.startAnimation(fadeOut);
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
-                helloText.animate().rotation(360f).setDuration(4000);
             }
         });
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                helloText.animate().rotation(-0).setDuration(2000);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                helloText.startAnimation(fadeIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        helloText.startAnimation(fadeIn);
 
         vibrateButton.setOnClickListener(v -> {
             Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
@@ -66,4 +87,8 @@ public class Task2Fragment extends Fragment {
 
         return root;
     }
+
+    private void rotateAnimation() {
+    }
+
 }
